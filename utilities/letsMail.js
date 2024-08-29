dcd3a5e251f23cc8e141172efc3192861555d940
Instructor <dodyDev311@gmail.com>
2024-08-29 19:21:01 +0530

    mail

--- utilities/letsMail.js
+++ utilities/letsMail.js
@@ -3,6 +3,13 @@
 // ojtgypwgofdwrpmh
 
 const nodemailer = require("nodemailer");
+const ejs = require("ejs");
+const path = require("path");
+const fs = require("fs");
+
+// fs module  => file system
+// path module => path of the file
+ 
 
 // access env: 
 const dotenv = require("dotenv");
@@ -19,16 +26,43 @@ const letsMail =  async (options) =>{
     let taransporter = nodemailer.createTransport({
         service: "gmail",
         auth: {
-            user: process.env.gmail,
-            pass: process.env.password
+            user: "abhis.me@gmail.com",
+            pass: "ojtgypwgorpmh"
         }
     })
 
+    // ejs template
+    
+      const templatePath = path.join(__dirname, `../templates/mail3.html`)
+
+    // const templatePath = path.join(__dirname, "..", "templates", "mail3.html");
+
+    //   const htmlTemplate = await ejs.renderFile(templatePath, {name: "Naman", otp: "1234"});
+    let htmlString = fs.readFileSync(templatePath, "utf-8");
+    htmlString = htmlString.replaceAll("{{name}}", "Naman");
+    htmlString = htmlString.replaceAll("{{link}}", "https://www.google.com");
+    
+    //  for attachments: 
+    const attachmentPath1 = path.join(__dirname, `../static/cover.png`)
+    const attachmentPath2 = path.join(__dirname, `../static/javascriptNotes.pdf`)
+
+
     const mailOptions = {
-        from: process.env.password,
-        to: options.to,
-        subject: options.subject,
-        text: options.text
+        from: "abhishekchoudhary.me@gmail.com",
+        to: "nama@gmail.com",
+        subject: "Registartion email with attachments",
+        html: htmlString,
+        attachments: [
+            {
+                filename: "cover.png",
+                path: attachmentPath1
+
+            },
+            {
+                filename: "javascriptNotes.pdf",
+                path: attachmentPath2
+            }
+        ]
     }
 
     try{
@@ -41,7 +75,13 @@ const letsMail =  async (options) =>{
 }
 
 
+// letsMail()
+
 
 
 
 module.exports = letsMail;
+
+
+
+// ../
--- zzzz.js
+++ zzzz.js
@@ -1,12 +1,55 @@
 
 
-try{
-     setTimeout(()=>{
-         console.log(a);
-     },2000)
- }
- catch(err){
-         console.log("Cath: Error caught", err.message);
- }
+// try{
+//      setTimeout(()=>{
+//          console.log(a);
+//      },2000)
+//  }
+//  catch(err){
+//          console.log("Cath: Error caught", err.message);
+//  }
 
- console.log("End of code")
+//  console.log("End of code")
+
+
+
+//  let m = 1, p = 100
+// for (let i = m; i<=p; i++) {
+//       let n=i
+//  let countofDigits = 0
+//  while(n>0) {
+//   countofDigits++
+//   n = parseInt(n/10)
+//  }
+//  n = i
+//  let sumofcube = 0
+//  while(n>0) {
+//   let last = n%10
+//   sumofcube = sumofcube + last ** countofDigits
+//   n=parseInt(n/10)
+//  }
+//  if(sumofcube == i) {
+//   console.log(i)
+//  }
+// }	
+
+
+// if (num < 2) {
+//     return false;
+//  }
+//     for (let i = 2; i <= Math.sqrt(num); i++) {
+//     if (num % i === 0) {
+//        return false;
+//     }
+    
+//     return true;
+//     }
+//     function findPrimeSum(n) {
+//     for(let i = 2, j = n - 2; i <= j; i++, j--) {
+//       if(isPrime(i) && isPrime(j)) {
+//     return [i, j];
+//     }
+//     }
+//     return [-1];
+//     }
+//     // sum of prime
